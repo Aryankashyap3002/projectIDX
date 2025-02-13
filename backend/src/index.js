311698c3a7c72efa92e7f204354342d5f8ffc226
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-02-13 14:22:15 +0530

    Added quering functionalites using react query

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,19 @@
-import express, { urlencoded } from 'express';
+import express from 'express';
 import cors from 'cors'
 import { PORT } from './config/serverConfig.js'
+import apiRouter from './routes/index.js'
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
 app.get('/ping', (req, res) => {
     return res.json({message: "pong"});
-} )
+})
+
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
     console.log("Server is running on port: ", PORT);
---
+++ backend/src/routes/index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import V1Router from './v1/index.js'
+
+const router = express.Router();
+
+
+router.use('/v1', V1Router);
+
+export default router;
