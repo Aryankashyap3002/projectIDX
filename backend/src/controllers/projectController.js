8e44798e8430bfb09921c6e621d8b2bde1525e91
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-02-16 18:28:32 +0530

    Added folder structure

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -1,26 +1,17 @@
-import util from 'util';
-import child_process from 'child_process';
-import fs from 'fs/promises';
-import uuid4 from 'uuid4';
-import { REACT_PROJECT_COMMAND } from '../config/serverConfig.js';
-
-
-const execPromisified = util.promisify(child_process.exec);
+import { createProjectService, getProjectTreeService } from "../service/projectservice.js";
 
 export const createProjectController = async (req, res) => {
 
-   // create projecct on the project repo such that every project has a unique id.
-
-    const projectId = uuid4();
-    console.log("project Id: ", projectId);
-
-    await fs.mkdir(`./projects/${projectId}`);
-
-    // now call the create vite command for creating vite project on newly created project folder
-
-    const response = await execPromisified(REACT_PROJECT_COMMAND, {
-        cwd: `./projects/${projectId}` 
-    })
+    const projectId = await createProjectService();
 
     return res.json({ message: 'project is created', data: projectId });
- }
+}
+
+export const getProjectTree = async (req, res) => {
+    const tree = await getProjectTreeService(req.params.projectId);
+    return res.status(200).json({
+        data: tree,
+        success: true,
+        message: "Successfully fetched the tree"
+    })
+}
---
+++ backend/src/service/projectservice.js
@@ -0,0 +1,30 @@
+import uuid4 from "uuid4";
+import fs from 'fs/promises';
+import { REACT_PROJECT_COMMAND } from "../config/serverConfig.js";
+import { execPromisified } from "../utils/exceUtillity.js";
+import directoryTree from "directory-tree";
+import path from 'path'
+
+
+export const createProjectService = async () => {
+    // create projecct on the project repo such that every project has a unique id.
+
+    const projectId = uuid4();
+    console.log("project Id: ", projectId);
+
+    await fs.mkdir(`./projects/${projectId}`);
+
+    // now call the create vite command for creating vite project on newly created project folder
+
+    const response = await execPromisified;(REACT_PROJECT_COMMAND, {
+        cwd: `./projects/${projectId}` 
+    });
+
+    return projectId;
+}
+
+export const getProjectTreeService = (projectId) => {
+    const projectPath = path.resolve(`./projects/${projectId}`);
+    const tree = directoryTree(projectPath);
+    return tree;
+}
