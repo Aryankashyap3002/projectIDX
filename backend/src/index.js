24959054b025f99b9e5e132f6576572e0402e416
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-02-18 00:19:36 +0530

    Added socket for file reading

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,28 @@
 import express from 'express';
 import cors from 'cors'
+import { createServer } from 'node:http';
 import { PORT } from './config/serverConfig.js'
+import { Server } from 'socket.io';
 import apiRouter from './routes/index.js'
+import chokidar from 'chokidar';
+// import path from 'node:path';
+import { handleEditorSocketEvent } from '../projects/78ea223f-b4a2-4bc5-9779-3ae0957bb1e2/sandbox/src/socketHandler/editorHandler.js';
+// import queryString from 'query-string';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        method: ["GET", "POST"]
+    }
+});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
+
+
 
 app.get('/ping', (req, res) => {
     return res.json({message: "pong"});
@@ -15,6 +30,39 @@ app.get('/ping', (req, res) => {
 
 app.use('/api', apiRouter);
 
-app.listen(PORT, () => {
+const editorNamespace = io.of('/editor');
+
+editorNamespace.on("connection", (socket) => {
+    console.log("editor connected");
+
+    let projectId = socket.handshake.query.projectId; // projectId from backend coming through socket pipeline
+
+
+    if(projectId) {
+        var watcher = chokidar.watch(`./projects/${projectId}`, {
+            ignored: (path) => path.includes("node_modules"),
+            persistent: true, // keeps the watcher in running state till the time app is running
+            awaitWriteFinish: {
+                stabilityThreshold: 2000, // ensure the stability of files before triggering the event
+            },
+            ignoreInitial: true, // ignore the intial files in the directory
+
+        });
+
+        watcher.on("all", (event, path) => {
+            console.log(path, event);
+        })
+    }
+
+   handleEditorSocketEvent(socket); 
+
+    // socket.on('disconnect', async () => {
+    //     await watcher.close();
+    //     console.log("editor disconnected");
+    // })
+
+})
+
+server.listen(PORT, () => {
     console.log("Server is running on port: ", PORT);
 })
