d063b5ec2340db524d57c89536427a6aa65f69d5
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-04-08 16:49:37 +0530

    Added middlewares

--- backend/src/routes/v1/projects.js
+++ backend/src/routes/v1/projects.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createProjectController, getProjectTree } from '../../controllers/projectController.js';
+import { isAuthenticated } from '../../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/', createProjectController);
+router.post('/', isAuthenticated, createProjectController);
 
-router.get('/:projectId/tree', getProjectTree);
+router.get('/:projectId/tree', isAuthenticated, getProjectTree);
 
 export default router;
